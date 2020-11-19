faeba6d2dfcc742d7a2fdc4ec68d6a7d03a3ea9d
anubhavg <anubhav.garg@netapp.com>
2020-11-19 12:29:42 +0530

    small project

--- src/testfolder/main.go
+++ src/testfolder/main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"google.golang.org/api/compute/v1"
+	"time"
 )
 	const project = "western-notch-185412"
 	const region = "us-central1"
@@ -61,25 +62,25 @@ func main() {
 		Process: "dsfb",
 		Laafa: "dsfbh",
 	}
-	response := first.func1()
-	fmt.Println(*response,11)
+	lava := first.func1()
+	fmt.Println(*lava,11)
 	sum,r := add()
 	fmt.Println(sum,r)
-	//finished := make(chan simple)
-	//go createnet(finished, namea)
-	//response := <-finished
-	//ad,_ := json.Marshal(response)
-	//fmt.Println(string(ad),2)
-	//time.Sleep(20 * time.Second)
-	//finished1 := make(chan simple)
-	//finished2 := make(chan simple)
-	//go createSubnet(finished1, "subnetwork", "10.0.1.0/24", response.TargetLink)
-	//go createSubnet(finished2, "subnetwork1", "10.0.2.0/25", response.TargetLink)
-	//response1 := <-finished1
-	//response2 := <-finished2
-	//ad1,_ := json.Marshal(response1)
-	//fmt.Println(string(ad1),2)
-	//ad2,_ := json.Marshal(response2)
-	//fmt.Println(string(ad2),2)
+	finished := make(chan simple)
+	go createnet(finished, namea)
+	response := <-finished
+	ad,_ := json.Marshal(response)
+	fmt.Println(string(ad),2)
+	time.Sleep(20 * time.Second)
+	finished1 := make(chan simple)
+	finished2 := make(chan simple)
+	go createSubnet(finished1, "subnetwork", "10.0.1.0/24", response.TargetLink)
+	go createSubnet(finished2, "subnetwork1", "10.0.2.0/25", response.TargetLink)
+	response1 := <-finished1
+	response2 := <-finished2
+	ad1,_ := json.Marshal(response1)
+	fmt.Println(string(ad1),2)
+	ad2,_ := json.Marshal(response2)
+	fmt.Println(string(ad2),2)
 
 }
